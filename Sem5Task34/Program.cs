ed35f7294095985f46c3c533a777aa15d7e3f72f
Mariya <Maryblinova@mail.ru>
2023-01-03 22:07:05 +0300

    Sem5Task34, Sem5Task36

---
+++ Sem5Task34/Program.cs
@@ -0,0 +1,47 @@
+﻿
+int[] array = GenArray(123, 100, 999);
+int result=0;
+PrintArray(array);
+for (int i=0;i<array.Length;i++) {
+    if (isEver(array[i])) {
+        result++;
+    }
+}
+
+PrintData("Количество четных чисел в массиве", result);
+
+//заполнение массива
+int[] GenArray(int len, int minValue, int maxValue)
+{
+        
+    int[] outArray = new int[len];
+    
+    for (int i = 0; i < len; i++)
+    {
+        outArray[i] = new Random().Next(minValue, maxValue);
+    }
+    return outArray;
+}
+
+
+void PrintData(string res, int value)
+{
+    Console.WriteLine();
+    Console.WriteLine(res + value);
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("содержимое массива");
+    if (array!=null && array.Length>0) {
+        for(int i=0;i<array.Length;i++)
+        PrintData("элемент "+ i +"=", array[i]);
+    }
+}
+
+
+Boolean isEver(int value) {
+    return value %2==0;
+}
+
+
---
+++ Sem5Task36/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] array = GenArray(123, -100, 100);
+PrintArray(array);
+
+int result = SumOdd(array);
+Console.WriteLine();
+PrintData("Сумма элементов, стоящих на нечетных позициях", result);
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] outArray = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        outArray[i] = new Random().Next(minValue, maxValue);
+    }
+    return outArray;
+}
+
+void PrintData(string res, int value)
+{
+    Console.WriteLine(res + value);
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ",");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+}
+
+int SumOdd(int[] array)
+{
+    int res = 0;
+    int i =1;
+    while (i < array.Length)
+    {
+        res+=array[i];
+        i = i+2;
+        {
+            res++;
+        }
+    }
+    return res;
+}
