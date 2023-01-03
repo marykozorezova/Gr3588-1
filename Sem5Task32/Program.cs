28d6b44ef3bda70db3f9c9bbe05dba8b9cc6d73d
Mariya <Maryblinova@mail.ru>
2023-01-03 17:06:44 +0300

    Sem5Task31, Sem5Task32, Sem5Task33, Sem5Task35, Sem5Task37, Sem5Task34, Sem5Task36, Sem5Task38

---
+++ Sem5Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿    int[] array = GenArray(10,-9,9);
+    Print1Arr(array);
+    int[] newArray = InverseArr(array);
+    Print1Arr(newArray);
+    
+
+    int[] GenArray(int len, int minValue, int maxValue)
+    {
+        Random rnd = new Random();
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = new Random().Next(minValue, maxValue + 1);
+        }
+        return arr;
+    }
+
+  
+    
+
+    // Печать одномерного массива
+    void Print1Arr(int[] arr)
+    {
+        for (int i = 0; i < arr.Length -1; i++)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        Console.WriteLine(arr[arr.Length - 1]);
+    
+    }
+
+    int[] InverseArr(int[] arr)
+    {
+        int[] outArr = new int[arr.Length];
+        for(int i=0;i<arr.Length;i++)
+        {
+            outArr[i]=arr[i]*(-1);
+        }
+        return outArr;
+    }
+
---
+++ Sem5Task33/Program.cs
@@ -0,0 +1,50 @@
+﻿int[] array = GenArray(10, -9, 10);
+Print1DArr(array);
+int num = ReadData("Введите число");
+int result = SearchElem(array, num);
+
+PrintData("" + result);
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void Print1DArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1]);
+}
+
+int SearchElem(int[] arr, int elem)
+{
+    int res = -1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == elem)
+        {
+            res = i;
+            break;
+        }
+    }
+    return res;
+}
